af3f8c510fcfd1787a939805b656dbcae8408f96
SabithHaneef <sabith.th.07@gmail.com>
2019-10-02 17:40:42 +0530

    fix minor bug

--- part1/src/index.js
+++ part1/src/index.js
@@ -17,7 +17,7 @@ const Content = props => {
     <div>
       <Part part={props.parts[0]} />
       <Part part={props.parts[1]} />
-      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
