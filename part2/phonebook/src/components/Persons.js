d6500229512cf9e3ec5ba61e9a5823bfa8bf086f
SabithHaneef <sabith.th.07@gmail.com>
2019-10-08 20:46:11 +0530

    fix person.key

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -4,7 +4,7 @@ import Person from "./Person";
 const Persons = ({ persons, onDelete }) => {
   const buildPersonsList = persons =>
     persons.map(person => (
-      <Person key={person.name} person={person} onDelete={onDelete} />
+      <Person key={person.id} person={person} onDelete={onDelete} />
     ));
 
   return <div>{buildPersonsList(persons)}</div>;
