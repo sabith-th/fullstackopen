f53d06b820039f2ad2409169e77c895574dc4c8b
SabithHaneef <sabith.th.07@gmail.com>
2019-10-22 18:54:56 +0530

    completed exercise 6.3 to 6.8

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
+
+const App = ({ store }) => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteForm store={store} />
+      <AnecdoteList store={store} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = ({ store }) => {
+  const addAnecdote = e => {
+    e.preventDefault();
+    const content = e.target.anecdote.value;
+    e.target.anecdote.value = "";
+    store.dispatch(createAnecdote(content));
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default AnecdoteForm;
